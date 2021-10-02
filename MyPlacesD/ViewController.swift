7c12ed65aa2110c8c1fb1d3b36701c1a79ba8d55
Daria <daria.gurina@gmail.com>
2021-10-02 11:31:51 +0200

    Initial Commit

---
+++ MyPlacesD/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyPlacesD
+//
+//  Created by Yumatova Daria on 02.10.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
