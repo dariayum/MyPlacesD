e81dff8535a055fdf22c6fcfd0f9116d61b726fc
Daria <daria.gurina@gmail.com>
2021-10-08 14:49:33 +0200

    Custom Table View Cell

---
+++ MyPlacesD/CustomTableViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  CustomTableViewCell.swift
+//  MyPlacesD
+//
+//  Created by Yumatova Daria on 08.10.2021.
+//
+
+import UIKit
+
+class CustomTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var ImageOfPlace: UIImageView!
+    @IBOutlet weak var NameLabel: UILabel!
+    @IBOutlet weak var locationLabel: UILabel!
+    @IBOutlet weak var typeLabel: UILabel!
+
+}
